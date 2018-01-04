e9d9f10d787a4c1fe99a84b607bbe0795833d731
Jaime Visser <jaimevisser@gmail.com>
2018-01-04 16:20:55 +0100

    Device and icons

---
+++ drivers/chargepoint/device.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const Homey = require('homey')
+const TNM = require('../../lib/tnm')
+
+class Chargepoint extends Homey.Device {
+    async onInit() {
+        this.updateDevice();
+        this.start_update_loop();
+    }
+
+    start_update_loop() {
+        setInterval(() => {
+            this.updateDevice();
+        }, 300000); //5 min
+    }
+
+    async updateDevice() {
+        let id = this.getData().id
+        let data = await TNM(id)
+
+        let connectors = data.connectors.length
+        let free = data.connectors.filter((conn) => conn.status == 0).length
+
+        console.log("connectors: " + connectors)
+        console.log("free: " + free)
+
+        await this.setCapabilityValue('connectors.total', connectors)
+        await this.setCapabilityValue('connectors.free', free)
+    }
+}
+
+module.exports = Chargepoint
