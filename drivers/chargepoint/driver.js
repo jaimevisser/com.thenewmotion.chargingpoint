9581805ec8cd1a705529362da2d04921e02fda30
Jaime Visser <jaimevisser@gmail.com>
2018-01-04 13:38:45 +0100

    Add driver and pairing

---
+++ drivers/chargepoint/driver.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const Homey = require('homey')
+const TNM = require('../../lib/tnm')
+
+class ChargepointDriver extends Homey.Driver {
+
+    onPairListDevices(data, callback) {
+
+        const Location = Homey.ManagerGeolocation
+
+        TNM.near(Location.getLatitude(), Location.getLongitude(), 1)
+            .then(function (points) {
+                let devices = points.map((point) => {
+                    return {
+                        name: point.address.trim() + ", " + point.city.trim() + " (" + point.provider.trim + ")",
+                        data: {
+                            id: point.id
+                        }
+                    }
+                })
+
+                callback(null, devices)
+            })
+            .catch((err) => callback(err, null))
+    }
+}
+
+module.exports = ChargepointDriver
